44e8288cfd0057b77a83a698a7f6efdad2b22c9e
Gabbytam <gtam2@fordham.edu>
2020-11-24 19:05:32 -0800

    completed part 2

--- src/MyList.js
+++ src/MyList.js
@@ -3,8 +3,34 @@ import './App.css'
 import ListItem from './ListItem.js'
 
 class MyList extends Component {
+
+  state = {
+    toDoItemsArray: this.props.theList
+  }
+
+  clearList = () => {
+    this.setState({
+      toDoItemsArray: [],
+      newItem: ''
+    })
+  }
+
+  newItemChange = (e) => {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem = (e) => {
+    //when a form is submitted, the whole page will reload, to prevent this, pass the onClick associated with the addItem method an event so that we can then call preventDefault on e
+    e.preventDefault();
+    this.setState({
+      toDoItemsArray: [...this.state.toDoItemsArray, this.state.newItem]
+    })
+  }
+
   render() {
-    let todoItems = this.props.theList.map((item, i) => (
+    let todoItems = this.state.toDoItemsArray.map((item, i) => (
       <ListItem doThis={item} key={'todo' + i} />
     ))
     return (
@@ -15,12 +41,13 @@ class MyList extends Component {
           {todoItems}
         </ul>
 
-        <div>
-          <input type='text' placeholder='Type an item here'></input>
-          <button id='addLi'>Add it!</button>
-        </div>
-    
-        <input type='Submit' value='Finished List!'></input>
+        <form>
+        {/* We can use onChange on the input field to trigger an event when the text in the box is changed. */}
+          <input type='text' placeholder='Type and item here' onChange={(event) => this.newItemChange(event)} value={this.state.newItem}></input>
+          <button onClick={(event) => this.addItem(event)}>Add item</button>
+        </form>
+
+        <button onClick={this.clearList}>Clear List</button>
         
       </div>
     )
